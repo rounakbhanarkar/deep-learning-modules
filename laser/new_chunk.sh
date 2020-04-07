617783aa020edb10ea36f68dc83aba44dda8ac36
rounakbhanarkar <63300375+rounakbhanarkar@users.noreply.github.com>
2020-04-07 21:34:08 +0530

    Update new_chunk.sh
    separates each chunk into different folders

--- laser/new_chunk.sh
+++ laser/new_chunk.sh
@@ -1,1 +1,10 @@
-separating individual chunks into different folders
+#!usr/bin/bash
+
+i=1
+while read Line
+do
+mkdir EH_$i
+echo "$Line" > en-hi_$i
+mv en-hi_$i EH_$i
+i=$((i+1))
+done < testfile
