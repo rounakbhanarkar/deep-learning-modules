7d722c4497daa092e69658ebaedc10107f77af65
rounakbhanarkar <63300375+rounakbhanarkar@users.noreply.github.com>
2020-04-07 21:50:36 +0530

    generating perplexity scores for multiples files

---
+++ LM/LM.sh
@@ -0,0 +1,29 @@
+#!usr/bin/bash
+
+cat H1
+cat H1 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
+cat H2
+cat H2 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
+cat H3
+cat H3 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
+cat H4
+cat H4 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
+cat H5
+cat H5 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
+cat H6
+cat H6 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
+cat H7
+cat H7 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
+cat H8
+cat H8 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
+cat H9
+cat H9 | ~/mosesdecoder-master/bin/query mono_3crore.blm.hi
+printf "\n"
